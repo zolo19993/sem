6bdd2d2706e5e3e179be592daedfe63d98673b73
igor kolesnikov <zolo19993@mail.ru>
2024-01-13 17:29:39 +0500

    add first task

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@
 // a = 9, b = -3 => да
 // a = -3, b = 9 => нет
 
-// тип_данных имя_переменной = значение;
-// camelCase, firstNumber, secondNumber
-int number = 9; // number = 9
-int result = number * number; // 81
+// Console.Readline() - получить информацию из строчки
+Console.Write("Ввести число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввести число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+// f = 25; s = 5: f == s*s, 25 == 5 * 5
+if(firstNumber == secondNumber * secondNumber) //  а является кв от числа b
+{
+    Console.WriteLine("Да, " + firstNumber + "да является кв. от числа " + secondNumber);
+}
+else // а не является кв от числа b
+{
+    Console.WriteLine("Нет, " + firstNumber + "не является кв. от числа " + secondNumber);
+}
